592a30613f5fd4014b3d34fbb5d2cee702087cf3
shash-786 <ssalian60@yahoo.com>
2024-11-15 21:33:17 +0530

    Updated ssh server

--- ssh/go.mod
+++ ssh/go.mod
@@ -3,6 +3,8 @@ module github.com/shash-786/Golang/ssh
 go 1.23.2
 
 require (
-	golang.org/x/crypto v0.28.0 // indirect
-	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/crypto v0.28.0
+	golang.org/x/term v0.25.0
 )
+
+require golang.org/x/sys v0.26.0 // indirect
--- ssh/server.go
+++ ssh/server.go
@@ -2,8 +2,11 @@ package ssh
 
 import (
 	"fmt"
+	"log"
+	"net"
 
 	"golang.org/x/crypto/ssh"
+	"golang.org/x/term"
 )
 
 func StartServer(serverPK []byte, userPubK []byte) error {
@@ -54,5 +57,83 @@ func StartServer(serverPK []byte, userPubK []byte) error {
 	}
 	config.AddHostKey(priv)
 
-	return nil
+	listner, err := net.Listen("tcp", "0.0.0.0:2022")
+	if err != nil {
+		return fmt.Errorf("net.Listen error: %v", err)
+	}
+
+	for {
+		ncon, err := listner.Accept()
+		if err != nil {
+			fmt.Printf("failed to accept incoming connection: %v", err)
+		}
+
+		conn, chans, reqs, err := ssh.NewServerConn(ncon, config)
+		if err != nil {
+			fmt.Printf("NewServerConn error: %v", err)
+		}
+
+		if conn != nil && conn.Permissions != nil {
+			log.Printf("logged in with key %s", conn.Permissions.Extensions["pubkey-fp"])
+		}
+
+		go ssh.DiscardRequests(reqs)
+		go handleConnection(conn, chans)
+	}
+}
+
+func handleConnection(conn *ssh.ServerConn, chans <-chan ssh.NewChannel) {
+	for newChannel := range chans {
+		// Channels have a type, depending on the application level
+		// protocol intended. In the case of a shell, the type is
+		// "session" and ServerShell may be used to present a simple
+		// terminal interface.
+		if newChannel.ChannelType() != "session" {
+			newChannel.Reject(ssh.UnknownChannelType, "unkown channel type")
+			continue
+		}
+
+		channel, requests, err := newChannel.Accept()
+		if err != nil {
+			log.Fatalf("Could not accept channel: %v", err)
+		}
+
+		go func(in <-chan *ssh.Request) {
+			for req := range in {
+				fmt.Printf("Request Type made by client: %s\n", req.Type)
+				switch req.Type {
+				case "pty-req":
+					createTerminal(conn, channel)
+				case "shell":
+					req.Reply(true, nil)
+
+				default:
+					req.Reply(false, nil)
+				}
+			}
+		}(requests)
+	}
+}
+
+func createTerminal(conn *ssh.ServerConn, channel ssh.Channel) {
+	termInstance := term.NewTerminal(channel, "> ")
+
+	go func() {
+		defer channel.Close()
+		for {
+			line, err := termInstance.ReadLine()
+			if err != nil {
+				fmt.Printf("ReadLinde error: %s", err)
+				break
+			}
+
+			switch line {
+			case "whoami":
+				termInstance.Write([]byte(fmt.Sprintf("You are: %s\n", conn.Conn.User())))
+			case "":
+			default:
+				termInstance.Write([]byte("Command not found\n"))
+			}
+		}
+	}()
 }
