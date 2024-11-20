ee4ccacfc9920044396185431182e03f912966e8
shash-786 <ssalian60@yahoo.com>
2024-11-20 15:57:51 +0530

    azure resource group created

--- mcrsft-azure/main.go
+++ mcrsft-azure/main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
 	"github.com/shash-786/Golang/ssh"
 )
 
+var LOCATION string = "westus"
+
 func main() {
 	var (
-		publicKey, token string
-		err              error
+		publicKey, subscriptionID string
+		token                     *azidentity.AzureCLICredential
+		err                       error
 	)
 
 	if publicKey, err = generatekeys(); err != nil {
@@ -18,12 +25,19 @@ func main() {
 		os.Exit(1)
 	}
 
+	ctx := context.Background()
 	if token, err = generateToken(); err != nil {
 		fmt.Printf("generatekeys error: %v", err)
 		os.Exit(1)
 	}
 
-	if err = launchInstance(); err != nil {
+	subscriptionID = os.Getenv("AZURE_SUBSCRIPTION_ID")
+	if subscriptionID == "" {
+		fmt.Println("No SubscriptionID found!")
+		os.Exit(1)
+	}
+
+	if err = launchInstance(ctx, token, subscriptionID, publicKey); err != nil {
 		fmt.Printf("launchInstance error: %v", err)
 		os.Exit(1)
 	}
@@ -38,12 +52,12 @@ func generatekeys() (string, error) {
 		os.Exit(1)
 	}
 
-	if err = os.WriteFile("./key/myKey.pem", priv, 0600); err != nil {
+	if err = os.WriteFile("./keys/myKey.pem", priv, 0600); err != nil {
 		fmt.Printf("priv writefile error: %v", err)
 		os.Exit(1)
 	}
 
-	if err = os.WriteFile("./key/myKey.pub", pub, 0644); err != nil {
+	if err = os.WriteFile("./keys/myKey.pub", pub, 0644); err != nil {
 		fmt.Printf("pub writefile error: %v", err)
 		os.Exit(1)
 	}
@@ -51,10 +65,26 @@ func generatekeys() (string, error) {
 	return string(pub), err
 }
 
-func generateToken() (string, error) {
-	return "", nil
+func generateToken() (*azidentity.AzureCLICredential, error) {
+	token, err := azidentity.NewAzureCLICredential(nil)
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
 }
 
-func launchInstance() error {
+func launchInstance(ctx context.Context, token *azidentity.AzureCLICredential, subscriptionID, publicKey string) error {
+	client, err := armresources.NewResourceGroupsClient(subscriptionID, token, nil)
+	if err != nil {
+		return fmt.Errorf("armresources.NewClient error: %v", err)
+	}
+
+	resource_grp_params := armresources.ResourceGroup{
+		Location: to.Ptr(LOCATION),
+	}
+	_, err = client.CreateOrUpdate(ctx, "go-azure-demo", resource_grp_params, nil)
+	if err != nil {
+		return fmt.Errorf("armresources.client error: %v", err)
+	}
 	return nil
 }
