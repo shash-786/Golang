26f805bf16df33f4cce61a6544329c544b9a37c5
shash-786 <ssalian60@yahoo.com>
2024-11-21 20:44:44 +0530

    vm Created

--- mcrsft-azure/go.mod
+++ mcrsft-azure/go.mod
@@ -3,11 +3,16 @@ module github.com/shash-786/Golang/mcrsft-azure
 go 1.23.2
 
 require (
-	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.16.0 // indirect
-	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.8.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.16.0
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.8.0
+	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute v1.0.0
+	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v2 v2.2.1
+	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources v1.2.0
+	github.com/shash-786/Golang/ssh v0.0.0-20241120125947-69d06c99ce1d
+)
+
+require (
 	github.com/Azure/azure-sdk-for-go/sdk/internal v1.10.0 // indirect
-	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v2 v2.2.1 // indirect
-	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources v1.2.0 // indirect
 	github.com/AzureAD/microsoft-authentication-library-for-go v1.3.2 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -16,5 +21,6 @@ require (
 	golang.org/x/crypto v0.29.0 // indirect
 	golang.org/x/net v0.31.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/term v0.26.0 // indirect
 	golang.org/x/text v0.20.0 // indirect
 )
--- mcrsft-azure/main.go
+++ mcrsft-azure/main.go
@@ -8,16 +8,20 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v2"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
 	"github.com/shash-786/Golang/ssh"
 )
 
 var (
-	LOCATION           = "westus"
-	RESOURCEGROUPNAME  = "go-azure-demo"
-	VIRTUALNETWORKNAME = "sample-virtual-network"
-	SUBTNETNAME        = "sample-subnet-network"
+	LOCATION             = "westus"
+	RESOURCEGROUPNAME    = "go-azure-demo"
+	VIRTUALNETWORKNAME   = "sample-virtual-network"
+	SUBTNETNAME          = "sample-subnet-network"
+	PUBLICIPADDRESSNAME  = "sample-public-ip-address-allocation"
+	SECURITYGROUPNAME    = "shash-security"
+	NETWORKINTERFACENAME = "salian-iiface"
 )
 
 func main() {
@@ -143,9 +147,196 @@ func launchInstance(ctx context.Context, cred *azidentity.AzureCLICredential, su
 		return err
 	}
 
-	_, err = subnetpollerResp.PollUntilDone(ctx, nil)
+	subnet_resp, err := subnetpollerResp.PollUntilDone(ctx, nil)
 	if err != nil {
 		return err
 	}
+
+	Public_IP_Address_Client, err := armnetwork.NewPublicIPAddressesClient(subscriptionID, cred, nil)
+	if err != nil {
+		return err
+	}
+
+	Public_IP_Address_Poller_Resp, err := Public_IP_Address_Client.BeginCreateOrUpdate(
+		ctx,
+		*ResourceGroupsClientCreateOrUpdateResponse.Name,
+		PUBLICIPADDRESSNAME,
+		armnetwork.PublicIPAddress{
+			Location: &LOCATION,
+			Properties: &armnetwork.PublicIPAddressPropertiesFormat{
+				PublicIPAllocationMethod: to.Ptr(armnetwork.IPAllocationMethodStatic),
+			},
+		},
+		nil)
+	if err != nil {
+		return err
+	}
+	Public_IP_Address_Resp, err := Public_IP_Address_Poller_Resp.PollUntilDone(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	Security_Groups_client, err := armnetwork.NewSecurityGroupsClient(subscriptionID, cred, nil)
+	if err != nil {
+		return err
+	}
+
+	Security_Groups_Poller_Resp, err := Security_Groups_client.BeginCreateOrUpdate(
+		ctx,
+		*ResourceGroupsClientCreateOrUpdateResponse.Name,
+		SECURITYGROUPNAME,
+		armnetwork.SecurityGroup{
+			Location: &LOCATION,
+			Properties: &armnetwork.SecurityGroupPropertiesFormat{
+				SecurityRules: []*armnetwork.SecurityRule{
+					{
+						Name: to.Ptr("allow-ssh"),
+						Properties: &armnetwork.SecurityRulePropertiesFormat{
+							SourceAddressPrefix:      to.Ptr("0.0.0.0/0"),
+							SourcePortRange:          to.Ptr("*"),
+							DestinationAddressPrefix: to.Ptr("0.0.0.0/0"),
+							DestinationPortRange:     to.Ptr("22"),
+							Protocol:                 to.Ptr(armnetwork.SecurityRuleProtocolTCP),
+							Access:                   to.Ptr(armnetwork.SecurityRuleAccessAllow),
+							Description:              to.Ptr("Allow ssh on port 22"),
+							Direction:                to.Ptr(armnetwork.SecurityRuleDirectionInbound),
+							Priority:                 to.Ptr(int32(1001)),
+						},
+					},
+				},
+			},
+		},
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	Security_Groups_Resp, err := Security_Groups_Poller_Resp.PollUntilDone(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	Interface_client, err := armnetwork.NewInterfacesClient(subscriptionID, cred, nil)
+	if err != nil {
+		return err
+	}
+
+	Interface_Poller_Resp, err := Interface_client.BeginCreateOrUpdate(
+		ctx,
+		*ResourceGroupsClientCreateOrUpdateResponse.Name,
+		NETWORKINTERFACENAME,
+		armnetwork.Interface{
+			Location: &LOCATION,
+			Properties: &armnetwork.InterfacePropertiesFormat{
+				NetworkSecurityGroup: &armnetwork.SecurityGroup{
+					ID: Security_Groups_Resp.ID,
+				},
+				IPConfigurations: []*armnetwork.InterfaceIPConfiguration{
+					{
+						Name: to.Ptr("ipConfig"),
+						Properties: &armnetwork.InterfaceIPConfigurationPropertiesFormat{
+							PrivateIPAllocationMethod: to.Ptr(armnetwork.IPAllocationMethodDynamic),
+							Subnet: &armnetwork.Subnet{
+								ID: subnet_resp.ID,
+							},
+							PublicIPAddress: &armnetwork.PublicIPAddress{
+								ID: Public_IP_Address_Resp.ID,
+							},
+						},
+					},
+				},
+			},
+		},
+		nil)
+	if err != nil {
+		return err
+	}
+
+	Interface_Resp, err := Interface_Poller_Resp.PollUntilDone(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Creating VM")
+
+	virtualMachinesClient, err := armcompute.NewVirtualMachinesClient(
+		subscriptionID,
+		cred,
+		nil,
+	)
+
+	parameters := armcompute.VirtualMachine{
+		Location: to.Ptr(LOCATION),
+		Identity: &armcompute.VirtualMachineIdentity{
+			Type: to.Ptr(armcompute.ResourceIdentityTypeNone),
+		},
+		Properties: &armcompute.VirtualMachineProperties{
+			StorageProfile: &armcompute.StorageProfile{
+				ImageReference: &armcompute.ImageReference{
+					// search image reference
+					// az vm image list --output table
+					// require ssh key for authentication on linux
+					Offer:     to.Ptr("0001-com-ubuntu-server-focal"),
+					Publisher: to.Ptr("canonical"),
+					SKU:       to.Ptr("20_04-lts-gen2"),
+					Version:   to.Ptr("latest"),
+				},
+				OSDisk: &armcompute.OSDisk{
+					Name:         to.Ptr("TEMP"),
+					CreateOption: to.Ptr(armcompute.DiskCreateOptionTypesFromImage),
+					Caching:      to.Ptr(armcompute.CachingTypesReadWrite),
+					ManagedDisk: &armcompute.ManagedDiskParameters{
+						StorageAccountType: to.Ptr(armcompute.StorageAccountTypesStandardLRS), // OSDisk type Standard/Premium HDD/SSD
+					},
+					// DiskSizeGB: to.Ptr[int32](100), // default 127G
+				},
+			},
+			HardwareProfile: &armcompute.HardwareProfile{
+				VMSize: to.Ptr(armcompute.VirtualMachineSizeTypes("Standard_B1s")), // VM size include vCPUs,RAM,Data Disks,Temp storage.
+			},
+			OSProfile: &armcompute.OSProfile{ //
+				ComputerName:  to.Ptr("sample-computer"),
+				AdminUsername: to.Ptr("sample-user"),
+				// require ssh key for authentication on linux
+				LinuxConfiguration: &armcompute.LinuxConfiguration{
+					DisablePasswordAuthentication: to.Ptr(true),
+					SSH: &armcompute.SSHConfiguration{
+						PublicKeys: []*armcompute.SSHPublicKey{
+							{
+								Path:    to.Ptr(fmt.Sprintf("/home/%s/.ssh/authorized_keys", "sample-user")),
+								KeyData: to.Ptr(publicKey),
+							},
+						},
+					},
+				},
+			},
+			NetworkProfile: &armcompute.NetworkProfile{
+				NetworkInterfaces: []*armcompute.NetworkInterfaceReference{
+					{
+						ID: Interface_Resp.ID,
+					},
+				},
+			},
+		},
+	}
+
+	pollerResponse, err := virtualMachinesClient.BeginCreateOrUpdate(
+		ctx,
+		*ResourceGroupsClientCreateOrUpdateResponse.Name,
+		"demo-virtual-machine",
+		parameters,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	_, err = pollerResponse.PollUntilDone(ctx, nil)
+	if err != nil {
+		return err
+	}
+	fmt.Println("VM Successfully Created")
+
 	return nil
 }
